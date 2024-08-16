8de5f58e8e41cd419a191a08a878ad2ae36a538e
Felipe Guimarães <146668592+Fezaoo@users.noreply.github.com>
2024-08-16 17:53:55 -0300

    Bug fixed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,67 +4,71 @@ import 'package:evo_movie/services/moviedb.dart';
 import '../models/movie.dart';
 
 class Home extends StatefulWidget {
-
   @override
   State<Home> createState() => _HomeState();
 }
 
-
-
 class _HomeState extends State<Home> {
-
-  
   @override
   void initState() {
     super.initState();
   }
 
-  late List<Movie> data = [Movie(title: '', releaseDate: '')];
   String search = '';
-  
+
   @override
   Widget build(BuildContext context) {
+    List<Movie> data =
+        ModalRoute.of(context)?.settings.arguments as List<Movie>;
 
-    data = ModalRoute.of(context)?.settings.arguments as List<Movie>;
     return Scaffold(
-      backgroundColor: Colors.white12,
-      appBar: AppBar(
         backgroundColor: Colors.white12,
-        title: Text(
-          'Evo Movie',
-          style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Colors.white12,
+          title: Text(
+            'Evo Movie',
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: SafeArea(
+        body: SafeArea(
           child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-             Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SearchBar(
-                    leading: Icon(Icons.search),
-                    onChanged: (query) {setState(() {
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                SearchBar(
+                  leading: Icon(Icons.search),
+                  onChanged: (query) {
+                    setState(() {
                       search = query;
-                    });},
+                    });
+                  },
+                ),
+                Text(
+                  'testano ${data[0].title}',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
-                  Text(
-                    'testano ${data[0].title}',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Text(
+                          data[index].title,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
                   ),
-                   
-                ],
-              )
-          ],
-        ),
-      )),
-    );
+                )
+                            ],
+                          ),
+              )),
+        ));
   }
 }
